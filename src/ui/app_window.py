b148a79ef2a44bf8348c31757533369f55e301d5
n2qzshce <67349049+n2qzshce@users.noreply.github.com>
2021-03-24 13:19:27 -0600

    Kivy attempt 2 (#17)
    * Added basic kivy build.
    
    * Let's try adding tree.
    
    * Adding glew deps appears to have done the trick?
    
    * Renaming to move to kivy.
    
    * Now let's see if we can get it to build remotely.
    
    * update requirements.txt
    
    * only build windows.
    
    * angle backend.
    
    * disable cli and change paramters a bit.
    
    * fix build command.
    
    * re-enable mac.
    
    * Explicit callout of deps and glew.
    
    * added osx spec file.
    
    * disable fail-fast.
    
    * remove unnecessary dependencies
    
    * extension fix.
    
    * update osx extension
    
    * Added feature parity.
    
    * Added feature parity.
    
    * alphabetized buttons.
    
    * Added async labels.

--- src/ui/app_window.py
+++ src/ui/app_window.py
@@ -1,10 +1,16 @@
 import asyncio
 import logging
-import threading
 from abc import ABC
 from typing import TextIO
 
-from PySimpleGUI import PySimpleGUI, TIMEOUT_EVENT
+from kivy.app import App
+from kivy.core.window import Window
+from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
+from kivy.uix.checkbox import CheckBox
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
 from src.ham.migration.migration_manager import MigrationManager
 from src.ham.radio_generator import RadioGenerator
@@ -22,11 +28,10 @@ class AsyncWrapper:
 		self.dangerous_ops_checkbox = None
 		self._buttons_disabled = False
 		self.radio_buttons = dict()
-		self.debug_toggle = None
 		return
 
-	def arm_dangerous(self, _):
-		if self.dangerous_ops_checkbox.get():
+	def arm_dangerous(self, *args):
+		if self.dangerous_ops_checkbox.active:
 			logging.warning("Dangerous operations enabled. Have you backed up your input CSVs?")
 		else:
 			logging.info("Dangerous operations disabled")
@@ -39,68 +44,74 @@ class AsyncWrapper:
 
 	def _refresh_buttons(self):
 		for button in self.buttons:
-			button.update(disabled=self._buttons_disabled)
+			button.disabled = self._buttons_disabled
 
 		for button in self.dangerous_buttons:
-			button.update(disabled=self._buttons_disabled or not self.dangerous_ops_checkbox.get())
+			button.disabled = self._buttons_disabled or not self.dangerous_ops_checkbox.active
 
 	def _submit_blocking_task(self, task_func):
-		logging.info("---Starting task---")
-		self._set_buttons_disabled(True)
+		task = asyncio.create_task(self._async_blocking_task(task_func))
+		task.add_done_callback(self._check_exceptions)
+		return
+
+	def _check_exceptions(self, task: asyncio.Task):
 		try:
-			task_func()
+			task.result()
 		except Exception as e:
 			logging.error(f"Fatal error while processing task. PLEASE send this to the project owners.", exc_info=True)
+
+	async def _async_blocking_task(self, task_func):
+		logging.info("---Starting task---")
+		self._set_buttons_disabled(True)
+		await task_func()
 		logging.info("---Task finished---")
-		self.dangerous_ops_checkbox.Update(value=False)
+		self.dangerous_ops_checkbox.active = False
 		self._set_buttons_disabled(False)
 
 	def display_about_info(self, event):
 		self._submit_blocking_task(self._display_about_info_async)
 
-	def _display_about_info_async(self):
+	async def _display_about_info_async(self):
 		self._radio_generator.info()
 
 	def wizard_cleanup(self, event):
 		self._submit_blocking_task(self._wizard_cleanup_async)
 
-	def _wizard_cleanup_async(self):
+	async def _wizard_cleanup_async(self):
 		self._wizard.cleanup()
 
-	def wizard_bootstrap(self, event):
+	def wizard_bootstrap(self, button):
 		self._submit_blocking_task(self._wizard_bootstrap_async)
 
-	def _wizard_bootstrap_async(self):
+	async def _wizard_bootstrap_async(self):
 		self._wizard.bootstrap(True)
 
 	def migrations(self, event):
 		self._submit_blocking_task(self._migrations_async)
 
-	def _migrations_async(self):
+	async def _migrations_async(self):
 		self._migrations.migrate()
 
 	def migration_backups(self, event):
 		self._submit_blocking_task(self._migration_backups_async)
 
-	def _migration_backups_async(self):
+	async def _migration_backups_async(self):
 		self._migrations.remove_backups()
 
 	def radio_generator(self, event):
 		self._submit_blocking_task(self._radio_generator_async)
 
-	def _radio_generator_async(self):
+	async def _radio_generator_async(self):
 		gen_list = []
 
 		for radio in self.radio_buttons.keys():
-			if self.radio_buttons[radio].get():
+			if self.radio_buttons[radio].active:
 				gen_list.append(radio)
 
 		self._radio_generator.radio_list = gen_list
 		self._radio_generator.generate_all_declared()
 
-	def log_level(self, event):
-		value = self.debug_toggle.get()
-
+	def log_level(self, _, value):
 		if value:
 			logging.root.setLevel(logging.DEBUG)
 			logging.debug("Debug logging enabled.")
@@ -110,194 +121,139 @@ class AsyncWrapper:
 			logging.info("Debug logging disabled")
 
 
-class AppWindow:
-	def __init__(self):
-		self._event_handler = EventHandler()
-		self._async_wrapper = AsyncWrapper()
-		PySimpleGUI.theme('DarkGrey6')
+class AppWindow(App):
+	text_log = None
+	_async_wrapper = None
 
-		button_pool_rows = []
-		layout = []
-		menu = [
-			['File', ['Exit']],
-			['Help', ['About...']]
-		]
-		layout.append([PySimpleGUI.Menu(menu_definition=menu)])
-		self._event_handler.add_hook('About...', self._async_wrapper.display_about_info)
-
+	def build(self):
+		# debug logging
+		# cleanup
+		# wizard bootstrap
+		# generate all declared
+		# help page
+		# Window.clearcolor = (0.15, 0.15, 0.15, 1)
 		buttons = []
 		dangerous_buttons = []
+		self._async_wrapper = AsyncWrapper()
 
-		radio_header = PySimpleGUI.Text(text='Radio List')
-		button_pool_rows.append([radio_header])
+		layout = BoxLayout(orientation='horizontal')
 
-		radio_checkboxes = dict()
+		button_pool = BoxLayout(spacing=10, orientation='vertical', size_hint=(0.2, 1), padding=[15, 15, 15, 15])
+		button_pool.size_hint_min_x = 200
+		button_pool.size_hint_max_x = 250
+
+		radio_select_buttons = dict()
 		radios = [
 			radio_types.DEFAULT,
-			radio_types.FTM400,
 			radio_types.D878,
-			radio_types.CS800,
 			radio_types.BAOFENG,
+			radio_types.CS800,
+			radio_types.FTM400,
 		]
 
-		radio_button_list = []
+		radio_header = Label(text='Radio List', size_hint=(0.8, 0.125), font_size=15, bold=True, halign='left')
+		radio_header.text_size = [150, None]
+		radio_header.size_hint_min_x = 150
+
+		button_pool.add_widget(radio_header)
 		for radio in radios:
-			radio_checkbox = PySimpleGUI.Checkbox(
-				text=radio_types.pretty_name(radio),
-				default=radio == radio_types.DEFAULT,
-				tooltip=f"Generate codeplug files for {radio_types.pretty_name(radio)}",
-			)
-			radio_checkboxes[radio] = radio_checkbox
-			radio_button_list.append([radio_checkbox])
+			radio_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.1))
 
-		self._async_wrapper.radio_buttons = radio_checkboxes
+			radio_label = Label(text=radio_types.pretty_name(radio), size_hint=(0.8, 1), font_size=11, halign='left')
+			radio_label.size_hint_min_x = 150
+			radio_checkbox = CheckBox(size_hint=(0.2, 1))
+			radio_checkbox.active = radio == radio_types.DEFAULT
 
-		button_pool_rows += radio_button_list
+			radio_layout.add_widget(radio_label)
+			radio_layout.add_widget(radio_checkbox)
 
-		horizontal_divider = PySimpleGUI.HorizontalSeparator(color='black', pad=(0, (0, 50)))
-		button_pool_rows.append([horizontal_divider])
+			radio_select_buttons[radio] = radio_checkbox
+			button_pool.add_widget(radio_layout)
 
-		arm_dangerous_checkbox = PySimpleGUI.Checkbox(
-			text='Enable Dangerous Operations',
-			default=False,
-			enable_events=True,
-			key='Enable Dangerous Operations',
-			tooltip='Allows destructive operations to be run.'
-		)
-		self._event_handler.add_hook(arm_dangerous_checkbox.Key, self._async_wrapper.arm_dangerous)
-		button_pool_rows.append([arm_dangerous_checkbox])
-		self._async_wrapper.dangerous_ops_checkbox = arm_dangerous_checkbox
+			radio_label.text_size = [150, None]
 
-		cleanup_button = PySimpleGUI.Button(
-			button_text='Run Cleanup',
-			size=(20, 2), enable_events=True,
-			disabled=True,
-			tooltip='Removes existing "in" and "out" folders.'
-		)
-		self._event_handler.add_hook(cleanup_button.get_text(), self._async_wrapper.wizard_cleanup)
-		button_pool_rows.append([cleanup_button])
+		self._async_wrapper.radio_buttons = radio_select_buttons
+		empty_buffer = BoxLayout(orientation='horizontal', size_hint=(1, 0.25))
+		button_pool.add_widget(empty_buffer)
+
+		arm_dangerous_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.125))
+		arm_dangerous_label = Label(text='Enable Dangerous Operations', size_hint=(0.8, 1), font_size=11)
+		arm_dangerous_checkbox = CheckBox(size_hint=(0.2, 1))
+		arm_dangerous_layout.add_widget(arm_dangerous_label)
+		arm_dangerous_layout.add_widget(arm_dangerous_checkbox)
+		button_pool.add_widget(arm_dangerous_layout)
+
+		cleanup_button = Button(text='Run Cleanup', size_hint=(1, 0.125), on_press=self._async_wrapper.wizard_cleanup)
+		button_pool.add_widget(cleanup_button)
 		buttons.append(cleanup_button)
 		dangerous_buttons.append(cleanup_button)
 
-		wizard_button = PySimpleGUI.Button(
-			button_text='Run Setup Wizard',
-			size=(20, 2),
-			enable_events=True,
-			disabled=True,
-			tooltip='Generates "in" and "out" folders, as well as some sample data.'
-		)
-		self._event_handler.add_hook(wizard_button.get_text(), self._async_wrapper.wizard_bootstrap)
-		button_pool_rows.append([wizard_button])
-		buttons.append(wizard_button)
-		dangerous_buttons.append(wizard_button)
+		wizard = Button(text='Run Setup Wizard', size_hint=(1, 0.125), on_press=self._async_wrapper.wizard_bootstrap)
+		button_pool.add_widget(wizard)
+		buttons.append(wizard)
+		dangerous_buttons.append(wizard)
 
-		migrations_button = PySimpleGUI.Button(
-			button_text='Migrate CSV to Latest',
-			size=(20, 2),
-			enable_events=True,
-			disabled=True,
-			tooltip='Adds and renames columns and files for the latest input version.'
-		)
-		self._event_handler.add_hook(migrations_button.get_text(), self._async_wrapper.migrations)
-		button_pool_rows.append([migrations_button])
-		buttons.append(migrations_button)
-		dangerous_buttons.append(migrations_button)
+		migrate = Button(text='Migrate CSV to Latest', size_hint=(1, 0.125), on_press=self._async_wrapper.migrations)
+		button_pool.add_widget(migrate)
+		buttons.append(migrate)
+		dangerous_buttons.append(migrate)
 
-		migration_backups_button = PySimpleGUI.Button(
-			button_text='Remove Migration Backups',
-			size=(20, 2),
-			enable_events=True,
-			disabled=True,
-			tooltip='Removes .bak file extensions from the "in" directory.'
-		)
-		self._event_handler.add_hook(migration_backups_button.get_text(), self._async_wrapper.migration_backups)
-		button_pool_rows.append([migration_backups_button])
-		buttons.append(migration_backups_button)
-		dangerous_buttons.append(migration_backups_button)
+		migrate_del_backups = Button(text='Remove Migration Backups', size_hint=(1, 0.125), on_press=self._async_wrapper.migration_backups)
+		button_pool.add_widget(migrate_del_backups)
+		buttons.append(migrate_del_backups)
+		dangerous_buttons.append(migrate_del_backups)
 
-		generate_button = PySimpleGUI.Button(
-			button_text='Create Radio Plugs',
-			size=(20, 2),
-			enable_events=True,
-			tooltip='Generates CSVs to import into your radio program.'
-		)
-		self._event_handler.add_hook(generate_button.get_text(), self._async_wrapper.radio_generator)
-		button_pool_rows.append([generate_button])
-		buttons.append(generate_button)
+		generate = Button(text='Create radio plugs', size_hint=(1, 0.125), on_press=self._async_wrapper.radio_generator)
+		button_pool.add_widget(generate)
+		buttons.append(generate)
 
-		debug_logging_checkbox = PySimpleGUI.Checkbox(
-			text='Enable Debug Logging',
-			key='Enable Debug Logging',
-			default=False,
-			enable_events=True,
-			tooltip="More logging. You generally don't need this."
-		)
-		self._event_handler.add_hook(debug_logging_checkbox.Key, self._async_wrapper.log_level)
-		self._async_wrapper.debug_toggle = debug_logging_checkbox
-		button_pool_rows.append([debug_logging_checkbox])
+		help_btn = Button(text='Help', size_hint=(1, 0.125), on_press=self._async_wrapper.display_about_info)
+		button_pool.add_widget(help_btn)
+		buttons.append(help_btn)
 
-		text_box = PySimpleGUI.Multiline(default_text='', disabled=True, size=(150, 25), font=('Courier', 10))
-		self.text_log = text_box
+		log_output = AnchorLayout(size_hint=(0.8, 1))
+
+		text_log = TextInput(font_name='RobotoMono-Regular', text='', size_hint=(1, 1), readonly=True, font_size=11)
+		log_output.add_widget(text_log)
+
+		layout.add_widget(button_pool)
+		layout.add_widget(log_output)
+
+		self.text_log = text_log
 
 		logger = logging.getLogger('radio_sync')
 		formatter = logging.Formatter(
 			fmt='%(asctime)s.%(msecs)03d %(levelname)7s %(filename).6s:%(lineno)3s:  %(message)s',
 			datefmt="%Y-%m-%d %H:%M:%S"
 		)
+
 		text_box_logger = TextBoxHandler(self.text_log)
 		handler = logging.StreamHandler(stream=text_box_logger)
 		handler.setFormatter(formatter)
+
+		debugger_layout = BoxLayout(orientation='horizontal', size_hint=(1, 0.1))
+
+		debugger_label = Label(text='Verbose logging', size_hint=(0.8, 1), font_size=11, halign='left')
+		debugger_checkbox = CheckBox(size_hint=(0.2, 1))
+		debugger_checkbox.bind(active=self._async_wrapper.log_level)
+		debugger_checkbox.active = False
+
+		debugger_layout.add_widget(debugger_label)
+		debugger_layout.add_widget(debugger_checkbox)
+		button_pool.add_widget(debugger_layout)
+
 		logger.setLevel(logging.INFO)
 		logger.addHandler(handler)
-
-		button_pool = PySimpleGUI.Column(layout=button_pool_rows, expand_y=True, vertical_alignment='center')
-		layout.append([button_pool, text_box])
-
+		Window.size = (1200, 500)
+		logging.info("Welcome to the ham radio sync app.")
+		self._async_wrapper.dangerous_ops_checkbox = arm_dangerous_checkbox
 		self._async_wrapper.buttons = buttons
 		self._async_wrapper.dangerous_buttons = dangerous_buttons
-		self._layout = layout
-		self.window = None
+		arm_dangerous_checkbox.bind(active=self._async_wrapper.arm_dangerous)
 
-	_cycle_time = 0.001
-
-	async def async_run(self):
-		self.window = PySimpleGUI.Window(title='Ham Radio Sync', layout=self._layout, finalize=True)
-		logging.info(f"Welcome to the ham radio sync app.")
-
-		while True:
-			event, values = self.window.read()
-			if event == PySimpleGUI.WIN_CLOSED or event == 'Exit':  # if user closes window or clicks cancel
-				break
-			else:
-				logging.debug(event)
-
-			threading.Thread(target=self._event_handler.run_hook, args=(event, values), daemon=True).start()
-
-		self.window.close()
-		return
-
-	async def window_read(self):
-		return self.window.read(timeout=self._cycle_time * 1000)
-
-	async def window_refresh(self, window):
-		while True:
-			await asyncio.sleep(0.01)
-			window.refresh()
-
-
-class EventHandler:
-	def __init__(self):
-		self._events = dict()
-
-	def add_hook(self, hook_name, func):
-		self._events[hook_name] = func
-
-	def run_hook(self, hook_name, event_data):
-		if hook_name == TIMEOUT_EVENT:
-			return
-		logging.debug(f"Running hook `{hook_name}`")
-		if hook_name in self._events.keys():
-			self._events[hook_name](event_data)
+		self._async_wrapper.dangerous_ops_checkbox = arm_dangerous_checkbox
+		self._async_wrapper.arm_dangerous(None)
+		return layout
 
 
 class TextBoxHandler(TextIO, ABC):
@@ -306,5 +262,6 @@ class TextBoxHandler(TextIO, ABC):
 		self.lock = None
 
 	def write(self, record):
-		self._text_log.print(record, end='')
+		self._text_log.text += record
 		return
+
