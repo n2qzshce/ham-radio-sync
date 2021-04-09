7d1d80f536456303c1606c8beafcca181e67d76c
n2qzshce <sw.au+github@pm.me>
2021-04-09 13:32:11 -0600

    Let's see if skip works.

--- test/test_version.py
+++ test/test_version.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import unittest
 
 import requests
@@ -16,6 +17,10 @@ class VersionTest(unittest.TestCase):
 		logging.critical(f"Version found: {radio_sync_version.version}")
 
 	def test_version_incremented(self):
+		ref = os.environ['GITHUB_REF']
+		is_master = ref == 'refs/heads/version-check-fix'
+		if is_master:
+			logging.info("Skipping version increment check on master.")
 		endpoint = "https://api.github.com/repos/n2qzshce/ham-radio-sync/tags"
 		result = requests.get(endpoint)
 		latest_version = result.json()[0]['name']
