a7017e818e09ad951c289dcf1e900c7e4c3aac97
n2qzshce <67349049+n2qzshce@users.noreply.github.com>
2021-04-09 14:01:45 -0600

    Version check fix (#27)
    * added master check

--- test/test_version.py
+++ test/test_version.py
@@ -1,4 +1,5 @@
 import logging
+import os
 import unittest
 
 import requests
@@ -16,9 +17,26 @@ class VersionTest(unittest.TestCase):
 		logging.critical(f"Version found: {radio_sync_version.version}")
 
 	def test_version_incremented(self):
+		ref = 'DEV'
+		headers = {}
+		if 'GITHUB_REF' in os.environ.keys():
+			ref = os.environ['GITHUB_REF']
+		if 'GITHUB_TOKEN' in os.environ.keys():
+			headers['Authorization'] = f"Bearer {os.environ['GITHUB_TOKEN']}"
+		else:
+			logging.critical('GITHUB_TOKEN not set')
+
+		logging.critical(f"Current ref is `{ref}`")
+		is_master = ref == 'refs/heads/master'
+		if is_master:
+			logging.critical("Skipping version increment check on master.")
 		endpoint = "https://api.github.com/repos/n2qzshce/ham-radio-sync/tags"
-		result = requests.get(endpoint)
-		latest_version = result.json()[0]['name']
+		result = requests.get(endpoint, headers=headers)
+		result_json = result.json()
+		logging.critical(f"0 not found in keys. Valid keys:`{result_json}`")
+		logging.critical(f"1 not found in keys. Valid keys:`{result_json[0]}`")
+		logging.critical(f"2 not found in keys. Valid keys:`{result_json[0]['name']}`")
+		latest_version = result_json[0]['name']
 		latest = Version.coerce(latest_version)
 		current = Version.coerce(radio_sync_version.version)
 		self.assertGreater(current, latest, "Version has not been incremented.")
